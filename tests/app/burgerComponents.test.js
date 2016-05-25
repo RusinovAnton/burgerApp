508e505e50358c7545a272f821255a3603022d3c
Rusinov.anton <rusinov.amgrade@gmail.com>
2016-05-25 13:04:06 +0300

    fix tests :3

--- tests/app/burgerComponents.test.js
+++ tests/app/burgerComponents.test.js
@@ -1,41 +1,41 @@
-const should = require('chai').should();
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 
 import BurgerComponent from '../../app/classes/components/BurgerComponent';
 import { Size, Stuff, Top } from '../../app/classes/components/Components';
 
 describe('Size', function () {
     it('should be instance of BurgerComponent', function () {
-        assert.equal(new Size(15,20) instanceof BurgerComponent, true);
+        expect(new Size(15,20)).to.be.instanceof(BurgerComponent);
     });
     it('should validate input of cost, cal', function(){
-            try { new Size(); } catch(e) {should.exist(e)}
-            try { new Size(34); } catch(e) {should.exist(e)}
-            try { new Size(34, 'test'); } catch(e) {should.exist(e)}
-            try { new Size('43234', 'test'); } catch(e) {should.exist(e)}
-            try { new Size(234, -123); } catch(e) {should.exist(e)}
-            try { new Size('test', -123); } catch(e) {should.exist(e)}
+        expect(()=>{ new Size(); }).to.throw(Error);
+        expect(()=>{ new Size(34); }).to.throw(Error);
+        expect(()=>{ new Size(34, 'test'); }).to.throw(Error);
+        expect(()=>{ new Size('43234', 'test'); }).to.throw(Error);
+        expect(()=>{ new Size(234, -123); }).to.throw(Error);
+        expect(()=>{ new Size('test', -123); }).to.throw(Error);
     });
     it('should take two positive numbers as arguments', function(){
-        new Size(20,20);
-        new Size(20,20.32);
+        expect(()=>{new Size(20,20)}).to.not.throw(Error);
+        expect(()=>{new Size(20,20.32)}).to.not.throw(Error);
     })
 });
 describe('Stuff', function () {
     it('should be instance of BurgerComponent', function () {
-        assert.equal(new Stuff(15,20) instanceof BurgerComponent, true);
+        expect(new Stuff(15,20)).to.be.instanceof(BurgerComponent);
     });
     it('should validate input of cost, cal', function(){
-        try { new Stuff(); } catch(e) {should.exist(e)}
-        try { new Stuff(34); } catch(e) {should.exist(e)}
-        try { new Stuff(34, 'test'); } catch(e) {should.exist(e)}
-        try { new Stuff('43234', 'test'); } catch(e) {should.exist(e)}
-        try { new Stuff(234, -123); } catch(e) {should.exist(e)}
-        try { new Stuff('test', -123); } catch(e) {should.exist(e)}
+        expect(()=>{ new Stuff(); }).to.throw(Error);
+        expect(()=>{ new Stuff(34); }).to.throw(Error);
+        expect(()=>{ new Stuff(34, 'test'); }).to.throw(Error);
+        expect(()=>{ new Stuff('43234', 'test'); }).to.throw(Error);
+        expect(()=>{ new Stuff(234, -123); }).to.throw(Error);
+        expect(()=>{ new Stuff('test', -123); }).to.throw(Error);
     });
     it('should take two positive numbers as arguments', function(){
-        new Stuff(20,20);
-        new Stuff(20,20.32);
+        expect(()=>{new Stuff(20,20)}).to.not.throw(Error);
+        expect(()=>{new Stuff(20,20.32)}).to.not.throw(Error);
     });
     it('should take third optional parameter - type to change type of stuff', function(){
         var testStuff = new Stuff(20,20,'ADD');
@@ -44,18 +44,18 @@ describe('Stuff', function () {
 });
 describe('Top', function () {
     it('should be instance of BurgerComponent', function () {
-        assert.equal(new Top(15,20) instanceof BurgerComponent, true);
+        expect(new Top(15,20)).to.be.instanceof(BurgerComponent);
     });
     it('should validate input of cost, cal', function(){
-        try { new Top(); } catch(e) {should.exist(e)}
-        try { new Top(34); } catch(e) {should.exist(e)}
-        try { new Top(34, 'test'); } catch(e) {should.exist(e)}
-        try { new Top('43234', 'test'); } catch(e) {should.exist(e)}
-        try { new Top(234, -123); } catch(e) {should.exist(e)}
-        try { new Top('test', -123); } catch(e) {should.exist(e)}
+        expect(()=>{ new Top(); }).to.throw(Error);
+        expect(()=>{ new Top(34); }).to.throw(Error);
+        expect(()=>{ new Top(34, 'test'); }).to.throw(Error);
+        expect(()=>{ new Top('43234', 'test'); }).to.throw(Error);
+        expect(()=>{ new Top(234, -123); }).to.throw(Error);
+        expect(()=>{ new Top('test', -123); }).to.throw(Error);
     });
     it('should take two positive numbers as arguments', function(){
-        new Top(20,20);
-        new Top(20,20.32);
+        expect(()=>{new Top(20,20)}).to.not.throw(Error);
+        expect(()=>{new Top(20,20.32)}).to.not.throw(Error);
     })
 });
--- tests/app/menu.test.js
+++ tests/app/menu.test.js
@@ -1,5 +1,6 @@
-const should = require('chai').should();
 const assert = require('chai').assert;
+const expect = require('chai').expect;
+
 
 import Menu from '../../app/classes/Menu';
 import BurgerComponent from '../../app/classes/components/BurgerComponent';
@@ -11,47 +12,43 @@ var mockMenu = new Menu();
 
 describe('Menu', function () {
     it('has to be constructor function', function () {
-        try {
-            new Menu();
-        } catch (err) {
-            should.not.exist(err);
-        }
+        expect(()=>{new Menu()}).to.not.throw(Error)
     });
     describe('addSize()', function () {
         it('should validate arguments', function () {
-                try { mockMenu.addSize(); } catch(e) {should.exist(e)}
-                try { mockMenu.addSize(1024, new Size(13, 37)); } catch(e) {should.exist(e)}
-                try { mockMenu.addSize('test', {cost: 123, cal: 12}) } catch(e) {should.exist(e)}
+            expect(()=>{mockMenu.addSize()}).to.throw(Error);
+            expect(()=>{mockMenu.addSize(1024, new Size(13, 37));}).to.throw(Error);
+            expect(()=>{mockMenu.addSize('test', {cost: 123, cal: 12})}).to.throw(Error);
         });
         it('is ok if item object\'s name is a string an instance of Size', function () {
-            try { mockMenu.addSize('test', new Size(20, 30)); } catch(e) {should.not.exist(e)}
+            expect(()=>{mockMenu.addSize('test', new Size(20, 30));}).to.not.throw(Error);
         });
     });
     describe('addStuff()', function () {
         it('should validate arguments', function () {
-                try {mockMenu.addStuff(); } catch(e) {should.exist(e)}
-                try {mockMenu.addStuff(1024, new Stuff(13, 37)); } catch(e) {should.exist(e)}
-                try {mockMenu.addStuff('test', {cost: 123, cal: 12}) } catch(e) {should.exist(e)}
+            expect(()=>{mockMenu.addStuff()}).to.throw(Error);
+            expect(()=>{mockMenu.addStuff(1024, new Stuff(13, 37))}).to.throw(Error);
+            expect(()=>{mockMenu.addStuff('test', {cost: 123, cal: 12})}).to.throw(Error);
         });
         it('is ok if item object\'s name is a string an instance of Stuff', function () {
-            try { mockMenu.addStuff('test', new Stuff(20, 30)); } catch (e) { should.not.exist(e); }
+            expect(()=>{ mockMenu.addStuff('test', new Stuff(20, 30))}).to.not.throw(Error);
         });
     });
     describe('addTop()', function () {
         it('should validate arguments', function () {
-                try { mockMenu.addTop(); }  catch(e) {should.exist(e)}
-                try { mockMenu.addTop(1024, new Top(13, 37)); }  catch(e) {should.exist(e)}
-                try { mockMenu.addTop('test', {cost: 123, cal: 12}) }  catch(e) {should.exist(e)}
+            expect(()=>{mockMenu.addTop();}).to.throw(Error);
+            expect(()=>{mockMenu.addTop(1024, new Top(13, 37));}).to.throw(Error);
+            expect(()=>{mockMenu.addTop('test', {cost: 123, cal: 12})}).to.throw(Error);
         });
         it('is ok if item object\'s name is a string an instance of Top', function () {
-            try { mockMenu.addTop('test', new Top(20, 30)); } catch (e) { should.not.exist(e); }
+            expect(()=>{mockMenu.addTop('test', new Top(20, 30));}).to.not.throw(Error);
         });
     });
     describe('menuList() getter', function () {
         it('must return object', function () {
             assert.typeOf(mockMenu.menuList, 'object');
         });
-        it('must have 3 own properties in it', function(){
+        it('must have 3 own properties in it', function () {
             assert.equal(_size(mockMenu.menuList), 3);
         });
         it('must has size, stuff, top items with cost: 20 and cal: 30', function () {
